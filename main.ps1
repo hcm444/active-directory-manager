e8c0b45059ffb19ef849f1535aa6f8e031f1037e
Henry Meier <32826270+hcm444@users.noreply.github.com>
2023-01-30 09:02:03 -0800

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -3,23 +3,59 @@ Import-Module ActiveDirectory
 # Create a new AD user
 Function New-ADUser {
   param ($username, $password, $firstname, $lastname)
-  $user = New-Object System.Security.Principal.NTAccount($username)
-  $userDN = (Get-ADUser $user).DistinguishedName
-  New-ADUser -SamAccountName $username -UserPrincipalName "$username@domain.com" -Name "$firstname $lastname" -GivenName $firstname -Surname $lastname -Enabled $True -PasswordNeverExpires $True -AccountPassword (ConvertTo-SecureString $password -AsPlainText -Force)
+
+  try {
+    # Create an NTAccount object with the specified username
+    $user = New-Object System.Security.Principal.NTAccount($username)
+    
+    # Get the distinguished name of the user
+    $userDN = (Get-ADUser $user).DistinguishedName
+    
+    # Create a new AD user with the specified details
+    New-ADUser -SamAccountName $username -UserPrincipalName "$username@domain.com" -Name "$firstname $lastname" -GivenName $firstname -Surname $lastname -Enabled $True -PasswordNeverExpires $True -AccountPassword (ConvertTo-SecureString $password -AsPlainText -Force)
+    
+    Write-Output "User $username created successfully."
+  } catch {
+    Write-Error "Error creating user $username: $_"
+  }
 }
 
 # Update an existing AD user
 Function Set-ADUser {
   param ($username, $firstname, $lastname)
-  $user = New-Object System.Security.Principal.NTAccount($username)
-  $userDN = (Get-ADUser $user).DistinguishedName
-  Set-ADUser $userDN -GivenName $firstname -Surname $lastname
+  
+  try {
+    # Create an NTAccount object with the specified username
+    $user = New-Object System.Security.Principal.NTAccount($username)
+    
+    # Get the distinguished name of the user
+    $userDN = (Get-ADUser $user).DistinguishedName
+    
+    # Update the specified user with the given first and last names
+    Set-ADUser $userDN -GivenName $firstname -Surname $lastname
+    
+    Write-Output "User $username updated successfully."
+  } catch {
+    Write-Error "Error updating user $username: $_"
+  }
 }
 
 # Delete an existing AD user
 Function Remove-ADUser {
   param ($username)
-  $user = New-Object System.Security.Principal.NTAccount($username)
-  $userDN = (Get-ADUser $user).DistinguishedName
-  Remove-ADUser $userDN
+  
+  try {
+    # Create an NTAccount object with the specified username
+    $user = New-Object System.Security.Principal.NTAccount($username)
+    
+    # Get the distinguished name of the user
+    $userDN = (Get-ADUser $user).DistinguishedName
+    
+    # Delete the specified user
+    Remove-ADUser $userDN
+    
+    Write-Output "User $username deleted successfully."
+  } catch {
+    Write-Error "Error deleting user $username: $_"
+  }
 }
